83cd6802616feaad6fe7014304a705209a916805
Jon Liauw <jliauw@sonobi.com>
2016-06-27 15:30:37 -0400

    Added directory/ies generation for better gamelog.csv placement in file system

--- stat_totals_scraper.py
+++ stat_totals_scraper.py
@@ -12,8 +12,8 @@ def run():
     years = []
     hold = []
     print '\n   This script pulls the totals stat table from basketball-reference.com'
-    print '   for a given year/s and stores it to your current directory in the format:'
-    print '   \"YEAR_NBA_TOTALS.csv\"\n'
+    print '   for a given year/s and stores it to a new directory \'year_stats_files\' in the format:'
+    print '   \"year_nba_totals.csv\"\n'
     print '   If you want to pull:'
     print '   * a single year, just type the year'
     print '   * from multiple years, separate each year by a space'
@@ -64,29 +64,29 @@ def run():
            
     years.sort()
     
-    if not os.path.exists('csv_files'):
-        os.makedirs('csv_files')
+    if not os.path.exists('year_stats_files'):
+        os.makedirs('year_stats_files')
     
     source = os.path.dirname(os.path.abspath(__file__))
-    destination = source + "/csv_files"
+    year_destination = source + "/year_stats_files"
             
     try:
         for i in range(0, len(years)):
             store_league_year_stats(years[i])
-            file_name = str(years[i]) + '_NBA_TOTALS.csv'
-            shutil.move(file_name,destination)
-            print '   + Successfully stored ' + str(years[i]) + '_NBA_TOTALS.csv'
+            file_name = str(years[i]) + '_nba_totals.csv'
+            shutil.move(file_name, year_destination)
+            print '   + Successfully stored ' + str(years[i]) + '_nba_totals.csv'
     except:
-        print '   - Failed to store ' + str(years[i]) + '_NBA_TOTALS.csv'
+        print '   - Failed to store ' + str(years[i]) + '_nba_totals.csv'
 
     print ''  
-# 
+#
 def store_league_year_stats(year):
     time.sleep(5)
     url = 'http://www.basketball-reference.com/leagues/NBA_' + str(year) + '_totals.html'
     url_request = requests.get(url)
     html = url_request.text
-    output = open(str(year) + '_NBA_TOTALS.csv', "w+")
+    output = open(str(year) + '_nba_totals.csv', "w+") 
     soup = BeautifulSoup(html, 'html.parser')
     stats_table = soup.find('table', {'class' : 'sortable  stats_table'}).find('tbody').findAll('tr')
 
@@ -122,28 +122,32 @@ def store_league_year_stats(year):
         output.write('\n')
 
     output.close()
-# 
+#
 def store_game_logs(player_url):
-    # time.sleep(10)
+    # time.sleep(5)
     url_request = requests.get(player_url)
     html = url_request.text
     soup = BeautifulSoup(html, 'html.parser')
-    output = open(player_url[-5:-1] + '_GAMELOG.csv', "w+")
+    pid = player_url[player_url.rfind('players') + 10 : -14]
+    file_name = str(pid) + '_' + player_url[-5 : -1] + '_gamelog.csv'
+    output = open(str(pid) + '_' + player_url[-5 : -1] + '_gamelog.csv', "w+")
     gl_table = soup.find('table', {'class' : 'sortable  row_summable stats_table'}).find('tbody').findAll('tr')
 
     game_stats, game_log = [], []
 
-    game_log.append(['GAME', 'DATE', 'AGE', 'TEAM', 'HOME/AWAY', 'OPP', 'RESULT', 'GS', 'MP', 'FGM', 'FGA', 'FG%', '3PM', '3PA', '3P%', 
+    game_log.append(['PID', 'GAME', 'DATE', 'AGE', 'TEAM', 'HOME/AWAY', 'OPP', 'RESULT', 'GS', 'MP', 'FGM', 'FGA', 'FG%', '3PM', '3PA', '3P%', 
     'FTM', 'FTA', 'FT%', 'ORB', 'DRB', 'TRB', 'AST', 'STL', 'BLK', 'TOV', 'PF', 'PTS', 'GMSC', '+/-', 'DFS'])
 
     for row in gl_table: 
         omit = 0
         for attribute in row.findAll('td'):
-            if omit != 0:
+            if omit == 0:
+                game_stats.append(pid)
+            else:
                 game_stats.append(attribute.string)
             omit = omit + 1
         
-        if(len(game_stats) > 1 and game_stats[0] != None):
+        if(len(game_stats) > 1 and game_stats[1] != None):
             game_log.append(game_stats)
         game_stats = []
 
@@ -155,25 +159,34 @@ def store_game_logs(player_url):
         output.write('\n')
 
     output.close()
+
+    if not os.path.exists('game_logs'):
+        os.makedirs('game_logs')
+    
+    if not os.path.exists('game_logs/' + pid):
+        os.makedirs('game_logs/' + pid)
+
+    source = os.path.dirname(os.path.abspath(__file__))
+    destination = source + '/game_logs/' + pid
+    shutil.move(file_name, destination)
+
 #
-def store_player_urls(year):
-    time.sleep(10)
+def grab_player_urls(year):
+    time.sleep(5)
     url = 'http://www.basketball-reference.com/leagues/NBA_' + str(year) + '_totals.html'
     url_request = requests.get(url)
     html = url_request.text
     soup = BeautifulSoup(html, 'html.parser')
     a_tags = soup.find('table', {'class' : 'sortable  stats_table'}).find('tbody').findAll('a')
     
-    count = 0
     for a_tag in a_tags:
         if 'players' in a_tag['href']:
             full_url = 'http://basketball-reference.com' + str(a_tag['href'])
             # if full_url not in player_url:            
             player_url.append(full_url)
-
 #
-def store_game_log_urls(url):
-    time.sleep(10)
+def grab_game_log_urls(url):
+    time.sleep(5)
     url_request = requests.get(url)
     html = url_request.text
     li_tags = BeautifulSoup(html, 'html.parser').findAll('li', {'class' : 'narrow'})
@@ -189,26 +202,28 @@ def store_game_log_urls(url):
     for game_log in game_log_url:
         print str(game_log)
 #
-def url_parse_player_key(url):
-    pass
 
 # ---------------- main ----------------
 
 # run()
-# store_player_urls(2016)
-# store_game_log_urls(player_url[92])
-store_league_year_stats(2016)
-# store_game_logs('http://www.basketball-reference.com/players/c/conlemi01/gamelog/2015/')
-
-# ---------------- notes ---------------
+# grab_player_urls(2016)
+# grab_game_log_urls(player_url[92])
+# store_league_year_stats(2016)
+store_game_logs('http://www.basketball-reference.com/players/c/conlemi01/gamelog/2015/')
 
 '''
 
 What's next:
     
+    Overview:
+    1. Grab player urls from totals table
+    2. Grab game log urls from player page
+    3. Store it via store_game_logs 
+
+    Immediate:
+    + Add directories/subdirectories to organize game_log files by player
+
     High priority:
-    + Create key for player
-    + Add delay where necessary
     + Make postgres connection
     + Store data to database instead of csv
     + Generate PER 36 stats (why is it 36, not 48...too much extrapolation?) <--- do this within postgres db
